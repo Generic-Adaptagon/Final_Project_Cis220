edc73791366df67c415be564cbd1e5b3a85095a2
Generic-Adaptagon <Zorme@startmail.com>
2025-04-22 16:30:32 -0700

    Tweaked file documentation's (comments)

--- mainMenu.c
+++ mainMenu.c
@@ -3,34 +3,41 @@
 
 void mainMenu () {
 
+
 int choice = 0; // for the user's choice
 
-
+/*enumerations to make adding options easy*/
 enum CHOICES {EXIT = 1};
 
+/*start of menu loop*/
 do {
 	
+	/*Menu options*/
 	printf("Menu: make a choice:\n");
 	printf("1. Exit Menu\n");
-	printf("2. other option FIXME\n"); //FIXME options
+	printf("2. other option FIXME\n"); //FIXME options--------------
 	
+	/*user choice*/
 	scanf (" %d", &choice);
 	
+	/*Switch statement for  user's choice*/
 	switch (choice) 
 	{
 		
-		
+		/*Prints exiting and the current menu*/
 		case EXIT:
 			printf("Exiting [MENU HERE]\n\n");
 			break; // end of EXIT
 			
+		/*error message and handeling*/	
 		default:
 			printf ("\nThat is not a valid option. Please try again\n");
-			FLUSH;
+			FLUSH; // flushes buffer
 		
 	}
 	
+	/*end of menu loop*/
 }  while (choice != EXIT);
 
+} // end of Menu
 
-}//mm
--- menuTemplate.c
+++ menuTemplate.c
@@ -4,37 +4,41 @@
 # include <stdio.h>
 # define FLUSH while (getchar() != '\n')
 
-void MenuTemplate () { // FIXME change function name
+void MenuTemplate () { // FIXME change function name--------------
 
 int choice = 0; // for the user's choice
 
-
+/*enumerations to make adding options easy*/
 enum CHOICES {EXIT = 1};
 
+/* start of menu*/
 do {
 	
+	/*Menu options*/
 	printf("Menu: make a choice:\n");
 	printf("1. Exit Menu\n");
-	printf("2. other option FIXME\n"); //FIXME options
+	printf("2. other option FIXME\n"); //FIXME options--------------
 	
+	/*user choice*/
 	scanf (" %d", &choice);
 	
+	/*Switch statement for  user's choice*/
 	switch (choice) 
 	{
 		
-		
+		/*Prints exiting and the current menu*/
 		case EXIT:
 			printf("Exiting [MENU HERE]\n\n");
 			break; // end of EXIT
 			
+		/*error message and handeling*/	
 		default:
 			printf ("\nThat is not a valid option. Please try again\n");
-			FLUSH;
+			FLUSH; // flushes buffer
 		
 	}
-	
+	/*only ends if the user's choice is to exit*/
 }  while (choice != EXIT);
 
-
-}//mm
+} // end of Menu
 
