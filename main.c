91b3d66e14e47fe3edebed6d4fbb2df215b8423a
Generic-Adaptagon <Zorme@startmail.com>
2025-04-22 15:42:11 -0700

    added main.c and the beginings of the main menu

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+# include "mainMenu.c"
+
+int main () {
+
+printf ("FIXME: in main I need data input\n");
+
+mainMenu();
+
+return 0;
+
+}
---
+++ mainMenu.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+void mainMenu () {
+
+int choice = 0; // for the user's choice
+
+
+enum CHOICES {EXIT = 1};
+
+do {
+	printf("This is the main menu, there is nothing here\n");
+	choice = EXIT;
+}  while (choice != EXIT);
+
+
+}//mm
