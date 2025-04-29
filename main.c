927f0a1c94528b439e64353d9349d159b4d92bfa
GokuJeans <luochinne@gmail.com>
2025-04-29 15:25:36 -0700

    Updated main.c

--- main.c
+++ main.c
@@ -1,10 +1,16 @@
 //testing 
 #include <stdio.h>
 # include "menu.h"
+#include "codeParse.h"
 //# include "menuTemplate.c"
 
 
 int main () {
+	
+	loadData("data/operating_systems.txt", 0);        // Load OS data
+    loadData("data/hypervisors.txt", 1); // Load Hypervisor data
+    loadData("data/related_software.txt", 2);   // Load Software data
+    loadData("data/products.txt", 3);    // Load Product data
 
 printf (" (main) FIXME:I need data input\n");
 
